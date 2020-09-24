24f932e1cd117a243c85364de44eab6356a3519d
Jack Ma <1791670972@qq.com>
2020-09-24 10:57:50 +0800

    添加工具包

---
+++ common/utils.py
@@ -0,0 +1,15 @@
+import requests
+
+
+def get_openid(code):
+    data = {
+        "appid": "wx4eaafb41b19cef49",
+        "secret": "32b54bef67778f1723a487e2d042acbc",
+        "js_code": code,
+        "grant_type": "authorization_code"
+    }
+
+    response = requests.get("https://api.weixin.qq.com/sns/jscode2session", params=data)
+    res_data = response.json()
+    if int(res_data['errcode']) == 0:
+        return res_data['openid']
